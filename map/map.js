8cf9d0d09a6b9f1c5bfb938c491ba580a8a86485
glzjuer <glzjuer@gmail.com>
2014-04-13 21:17:19 -0500

    2

--- map/map.js
+++ map/map.js
@@ -1,26 +1,6 @@
-
 // Note: This function requires that you consent to location sharing when
 // prompted by your browser.
-function myFunctionQuery(){
-  Parse.initialize("om9ynedsIy67rU9vfQh8IVR2vv0A6WnFz0jgWUrP", "mzPU7M8YQwD83alRhWwGtM9niEiDcSKs4mOKSNbp");
-  var GameScore = Parse.Object.extend("TechBathroom");
-  var query = new Parse.Query(GameScore);
-  query.equalTo("gender", "F")&&query.equalTo("floor",1);
-  query.find({
-    success: function(results) {
-      // alert("Successfully retrieved " + results.length + " scores.");
-      // Do something with the returned Parse.Object values
-      for (var i = 0; i < results.length; i++) { 
-        var object = results[i];
-        var Tech = new google.maps.LatLng(object.get('latitude'),object.get('longitude'));
-        locationArray[i] =  Tech;
-        
-      }
-  },
-    error: function(error) {
-      alert("Error: " + error.code + " " + error.message);
-  }
-});
+
 var map;
 
 var Tech109 = new google.maps.LatLng(42.058170, -87.675690);
@@ -145,7 +125,27 @@ function deleteFriends(){
   friendMarkers=[];
 }
 
-
+function myFunctionQuery(){
+  Parse.initialize("om9ynedsIy67rU9vfQh8IVR2vv0A6WnFz0jgWUrP", "mzPU7M8YQwD83alRhWwGtM9niEiDcSKs4mOKSNbp");
+  var GameScore = Parse.Object.extend("TechBathroom");
+  var query = new Parse.Query(GameScore);
+  query.equalTo("gender", "F")&&query.equalTo("floor",1);
+  query.find({
+    success: function(results) {
+      // alert("Successfully retrieved " + results.length + " scores.");
+      // Do something with the returned Parse.Object values
+      for (var i = 0; i < results.length; i++) { 
+        var object = results[i];
+        var Tech = new google.maps.LatLng(object.get('latitude'),object.get('longitude'));
+        locationArray[i] =  Tech;
+        
+      }
+  },
+    error: function(error) {
+      alert("Error: " + error.code + " " + error.message);
+  }
+});
+  
 function attachActivityMessage(marker, num){
   var message = ['<a href="../review_Page.html">Tech109</a>',
 		 '<a href="../review_Page.html">Tech209</a>',
