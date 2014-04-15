7733f7cf576472b22960bad9860530b2549961a8
xytan123 <xytan123@gmail.com>
2014-04-15 02:08:05 -0500

    default the stars display by set them to 3 REMEMBER TO INCLUDE jquery.raty.js!!!

--- Xiaoyang/MapView.js
+++ Xiaoyang/MapView.js
@@ -1,47 +1,78 @@
  $(window).on('load resize', function () {
-        $('body').css({
-            "padding-top": $(".navbar").height() + 30 + "px"
-        });
-    });
+     $('body').css({
+         "padding-top": $(".navbar").height() + 30 + "px"
+     });
+ });
 
-    $(document).ready(function () {
+ $(document).ready(function () {
 
-        var countChecked = function () {
-            // console.log("c");
-            var checked = $('input:checked');
-            var checkedID = [];
-            var checkselect ='';
-            // console.log("checkedID");
-            checked.each(function(){
-                checkedID.push(this.id);
-            });
+     initialize();
+     var countChecked = function () {
+         // console.log("c");
+         var checked = $('input:checked');
+         var checkedID = [];
+         var checkselect = '';
+         // console.log("checkedID");
+         checked.each(function () {
+             checkedID.push(this.id);
+         });
 
-            console.log(checkedID);
+         console.log(checkedID);
 
-            $.each(checkedID,function(index,value){
-                checkselect = checkselect+'.'+value;
-            });
-            console.log(checkselect);
+         $.each(checkedID, function (index, value) {
+             checkselect = checkselect + '.' + value;
+         });
+         console.log(checkselect);
 
-            var rows = $('table.table.table-striped tr');
-            console.log(rows);
+         var rows = $('table.table.table-striped tr');
+         console.log(rows);
 
-            var shows= rows.filter(checkselect);
-            console.log(shows);
-            shows.show();
-            rows.not(shows).hide();
-        };
+         var shows = rows.filter(checkselect);
+         console.log(shows);
+         shows.show();
+         rows.not(shows).hide();
+     };
 
-        $(":radio").change(countChecked);
+     $(":radio").change(countChecked);
 
-        $("#view-list").on('click',function(){
-            console.log("list");
-            $('#list').show();
-            $('#map').hide();
-        });
-        $("#view-map").on('click',function(){
-            console.log("map");
-            $('#map').show();
-            $('#list').hide();
-        })
-});
+     $("#view-list").on('click', function () {
+         console.log("list");
+         $('#list').show();
+         $('#map').hide();
+     });
+     $("#view-map").on('click', function () {
+         console.log("map");
+         $('#map').show();
+         $('#list').hide();
+     })
+
+
+     $("#x1").raty({
+         readOnly: true,
+         score: 3
+     });
+     // console.log($('tbody').children().length);
+
+     // $('tbody').children().each(function(){
+     //     $(this).attr({
+     //         data-toggle : "modal",
+     //         data-target : "#details"
+     //     });
+     // });
+
+ });
+
+
+ function initialize() {
+     var column_stars = $("#stars").index();
+     $('tbody').children().each(function () {
+         $(this).find('td').eq(column_stars).raty({
+             readOnly: true,
+             score: 3
+         });
+
+     })
+     $('.raty').raty();
+
+
+ }
